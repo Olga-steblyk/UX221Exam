5c0c26c162aea34786151c9fc0147385089234d3
Olga-steblyk <Olga-steblyk@users.noreply.github.com>
2024-04-19 20:30:16 +0000

    deploy: bd03b104551d2bb605ad9cc12bdc49d8e48fd1a7

--- wp-content/themes/johnny/customElements.js
+++ wp-content/themes/johnny/customElements.js
@@ -8,7 +8,7 @@ customElements.define("x-year", CopyrightYear);
 
 class TwoSidedMarket extends HTMLElement{
       connectedCallback(){
-            this.innerHTML = `<a href="side-a">Make & Sell</a>&nbsp;<a href="side-b">Browse & Buy</a>`;
+            this.innerHTML = `<a href="Make & Sell">Make & Sell</a>&nbsp;<a href="Browse & Buy">Browse & Buy</a>`;
       }
 }
 
