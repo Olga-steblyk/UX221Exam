f3d8858aebf0dee1159985d27dee8821c801f514
Olga-steblyk <olga.steblyk@gmail.com>
2024-04-19 16:09:22 -0400

    added stuff

--- wp-content/themes/johnny/customElements.js
+++ wp-content/themes/johnny/customElements.js
@@ -8,7 +8,7 @@ customElements.define("x-year", CopyrightYear);
 
 class TwoSidedMarket extends HTMLElement{
       connectedCallback(){
-            this.innerHTML = `<a href="side-a">Side A</a>&nbsp;<a href="side-b">Side B</a>`;
+            this.innerHTML = `<a href="side-a">Make & Sell</a>&nbsp;<a href="side-b">Browse & Buy</a>`;
       }
 }
 
