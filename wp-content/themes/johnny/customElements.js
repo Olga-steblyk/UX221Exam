bfba293501a0a4751f752df09c94d81b22a0c158
Olga-steblyk <Olga-steblyk@users.noreply.github.com>
2024-04-19 19:51:34 +0000

    deploy: c11249dc7513308adfb5f983b62b4bf9d40261c3

--- wp-content/themes/johnny/customElements.js
+++ wp-content/themes/johnny/customElements.js
@@ -5,3 +5,11 @@ class CopyrightYear extends HTMLElement{
 }
 
 customElements.define("x-year", CopyrightYear);
+
+class TwoSidedMarket extends HTMLElement{
+      connectedCallback(){
+            this.innerHTML = `<a href="side-a">Side A</a>&nbsp;<a href="side-b">Side B</a>`;
+      }
+}
+
+customElements.define("x-twosided", TwoSidedMarket);
